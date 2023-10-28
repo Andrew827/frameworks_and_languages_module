b7886476591028856cdb1178e85493f8ad3638d1
Andrew827 <96234773+Andrew827@users.noreply.github.com>
2023-10-28 19:19:09 +0000

    Item Dictionary complete, post working

--- myapp/app.js
+++ myapp/app.js
@@ -6,6 +6,44 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+const itemDictionary = {};
+
+function addItem(id, user_id, keywords, description, lat, lon, date_from) {
+  const newItem = {
+    "ID": id,
+    "user_id": user_id,
+    "keywords": keywords,
+    "description": description,
+    "lat": lat,
+    "lon": lon,
+    "date_from": date_from
+  };
+
+  itemDictionary[id] = newItem;
+}
+
+addItem(1, "user1234", ["hammer", "nails", "tools"], "A hammer and nails set", 1, 1, "2021-11-22T08:22:39.067408");
+
+app.get('/items',(req, res) => 
+{
+  res.json(itemDictionary)
+})
+
+
+app.post('/itemDictionary', (req,res) =>
+{
+  items.push(req.body)
+  res.status(201).json(req.body)
+})
+
+app.delete('/items/:ID', (req, res) => {
+  console.log("Delete" + req.params.ID)
+  res.status(204).json()
+} )
+
+
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
