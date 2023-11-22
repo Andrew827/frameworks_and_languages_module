cf306ad5c128d45d1c952f14b63e7e9fa02afdc1
Andrew827 <96234773+Andrew827@users.noreply.github.com>
2023-11-22 03:16:44 +0000

    Coded updated, 14 tests, passed. Optional functionality atttempted but removed due to repeated failure. Will continue later today. Should be working on client tomorrow

--- server/app.pl
+++ server/app.pl
@@ -1,31 +1,83 @@
 use Mojolicious::Lite -signatures;
-use JSON;
+use Mojo::Date;
+use List::Util qw(first);
 use Mojolicious::Plugin::SecureCORS;
 
-app->plugin('SecureCORS');
-app->routes->to('cors.origin' => '*');
+app->hook(before_dispatch => sub ($c) {
+    $c->res->headers->header('Access-Control-Allow-Origin' => '*');
+    $c->res->headers->header('Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS');
+    $c->res->headers->header('Access-Control-Allow-Headers' => 'Content-Type');
+    $c->res->headers->header('Access-Control-Max-Age' => '3600');
+});
 
 app->config(hypnotoad => {listen => ['http://*:8000']});
 
 my %items;
 my $next_id = 1;
 
-sub item ($user_id, $keywords, $description, $lat, $lon) {
-    my $id = $next_id++;
-    my $date_from = Mojo::Date->new->to_datetime;
+sub iso_datetime {
+    return Mojo::Date->new->to_datetime;
+}
+
+get '/' => sub ($c) {
+    $c->render(text => 'Welcome to the API!');
+};
+
+post '/item' => sub ($c) {
+    my $params = $c->req->json;
+    unless ($params->{user_id} && $params->{keywords} && $params->{description} && $params->{lat} && $params->{lon}) {
+        return $c->render(json => { error => 'Missing required fields' }, status => 200);
+    }
+
     my $new_item = {
-        id          => $id,
-        user_id     => $user_id,
-        keywords    => $keywords,
-        description => $description,
-        lat         => $lat,
-        lon         => $lon,
-        date_from   => $date_from,
+        id          => $next_id++,
+        user_id     => $params->{user_id},
+        keywords    => $params->{keywords},
+        description => $params->{description},
+        lat         => $params->{lat},
+        lon         => $params->{lon},
+        date_from   => iso_datetime(),  
     };
 
-    push @{$items{$id}}, $new_item;
-    return $new_item;
-}
+    push @{$items{$new_item->{id}}}, $new_item;
+    $c->render(json => $new_item, status => 201);
+};
+
+get '/items' => sub ($c) {
+    my @all_items = map { @$_ } values %items;
+    $c->render(json => \@all_items, status => 200);
+};
+
+get '/item/:id' => sub ($c) {
+    my $id   = $c->stash('id');
+    my $item = first { $_->{id} == $id } map { @$_ } values %items;
+
+    if ($item) {
+        $c->render(json => $item, status => 200);
+    } else {
+        $c->render(json => { error => 'Item not found' }, status => 404);
+    }
+};
+
+del '/item/:id' => sub ($c) {
+    my $id      = $c->stash('id');
+    my $deleted = remove_item($id);
+
+    if ($deleted) {
+        $c->rendered(204);
+    } else {
+        $c->render(json => { error => 'Item not found' }, status => 404);
+    }
+};
+
+options '/' => sub ($c) {
+    $c->rendered(204);
+};
+
+options '/item' => sub ($c) {
+    $c->rendered(405);
+};
+
 
 sub remove_item ($id) {
     if (exists $items{$id}) {
@@ -34,46 +86,9 @@ sub remove_item ($id) {
         for my $key (keys %items) {
             $_->{id} = $index++ for @{$items{$key}};
         }
-
         return 1;
     }
-
     return 0;
 }
 
-get '/' => sub ($c) {
-    $c->render(text => 'Hello World!');
-};
-
-get '/items' => sub ($c) {
-    my @all_items = map { @$_ } values %items;
-    $c->render(json => \@all_items);
-};
-
-post '/item' => sub ($c) {
-    my $params = $c->req->json;
-    unless ($params->{user_id} && $params->{keywords} && $params->{description} && $params->{lat} && $params->{lon}) {
-        return $c->render(json => { error => 'Missing required fields' }, status => 405);
-    }
-
-    my $new_item = item($params->{user_id}, $params->{keywords}, $params->{description}, $params->{lat}, $params->{lon});
-    $c->render(json => $new_item, status => 201);
-};
-
-get '/item/:id' => sub ($c) {
-    my $id   = $c->stash('id');
-    my $item = first { $_->{id} == $id } map { @$_ } values %items;
-    return $c->render(json => $item) if $item;
-    $c->render(json => { error => 'Item not found' }, status => 404);
-};
-
-del '/item/:id' => sub ($c) {
-    my $id      = $c->stash('id');
-    my $deleted = remove_item($id);
-
-    return $c->rendered(204) if $deleted;
-    $c->render(json => { error => 'Item not found' }, status => 404);
-};
-
 app->start('daemon', '-l', 'http://localhost:8000');
-
