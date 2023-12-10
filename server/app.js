cebec3d89ff5889ad02380fc611eb129d1438141
Andrew827 <96234773+Andrew827@users.noreply.github.com>
2023-12-10 01:13:08 +0000

    4 of 5 client tests now passing, i wasn't using the correct methods for cypress to know what to look for. Followed tutorials closely. For some reason the delete button isnt working at all. We it works but the test doesnt like it. I will work out it out later. Hoping tomorrow i can wipe up an independent client. I am already using an independent layout framework bulma. Now i know how to get my cypress working i should be able to get something running. I played around with the addittional tests for the server but they repeatedly fail so i hashed them back out.

--- server/app.js
+++ server/app.js
@@ -1,23 +1,21 @@
-const express = require('express');
-const app = express();
-const port = 8000;
-const cors = require('cors');
-const path = require('path');
+const express = require('express')
+const app = express()
+const port = 8000
+const cors = require('cors')
+const path = require('path')
+const geolib = require('geolib') //Filtering for location
 
-app.use(cors({ origin: '*' }));
-app.use(express.json());
+app.use(cors({ origin: '*' }))
+app.use(express.json())
 
+app.use(express.static(path.join(__dirname, 'client')))
 
-
-
-app.use(express.static(path.join(__dirname, 'client')));
-
-const items = {};
-let nextID = 1;
+const items = {}
+let nextID = 1
 
 function item(user_id, keywords, description, lat, lon) {
-  const id = nextID++;
-  const date_from = new Date().toISOString();
+  const id = nextID++
+  const date_from = new Date().toISOString()
   const newItem = {
     id,
     user_id,
@@ -26,122 +24,131 @@ function item(user_id, keywords, description, lat, lon) {
     lat,
     lon,
     date_from,
-  };
-
-  if (!items[id]) {
-    items[id] = [];
   }
 
-  items[id].push(newItem);
-  return newItem;
+  if (!items[id]) {
+    items[id] = []
+  }
+
+  items[id].push(newItem)
+  return newItem
 }
 
 function removeItem(id) {
   if (items[id]) {
-    delete items[id];
+    delete items[id]
     Object.keys(items).forEach((key, index) => {
       items[key].forEach((item) => {
-        item.id = index + 1;
-      });
-    });
+        item.id = index + 1
+      })
+    })
 
-    return true;
+    return true
   }
 
-  return false;
+  return false
 }
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+  res.send('Hello World!')
+})
 
 app.get('/hello', (req, res) => {
-  res.status(200).json({ message: 'Hello from the server!' });
-});
-
-
+  res.status(200).json({ message: 'Hello from the server!' })
+})
 
 app.get('/items', (req, res) => {
-  const allItems = Object.values(items).flat();
-  res.status(200).json(allItems);
-});
+  const allItems = Object.values(items).flat()
+  res.status(200).json(allItems)
+})
 
 app.post('/item', (req, res) => {
-  const { user_id, keywords, description, lat, lon } = req.body;
+  const { user_id, keywords, description, lat, lon } = req.body
 
   if (!user_id || !keywords || !description || !lat || !lon) {
-    res.status(405).json({ error: 'Missing required fields' });
+    res.status(405).json({ error: 'Missing required fields' })
   } else {
-    
-    const newItem = item(user_id, keywords, description, lat, lon);
-    res.status(201).json(newItem);
+    const newItem = item(user_id, keywords, description, lat, lon)
+    res.status(201).json(newItem)
   }
-});
+})
 
 app.get('/items/user/:user_id', (req, res) => {
-  const user_id = req.params.user_id;
-  const filteredItems = filterItemsByUser(user_id);
-  res.status(200).json(filteredItems);
-});
+  const user_id = req.params.user_id
+  const filteredItems = filterItemsByUser(user_id)
+  res.status(200).json(filteredItems)
+})
 
 app.get('/items/keywords/:keywords', (req, res) => {
-  const keywords = req.params.keywords.split(',');
-  const filteredItems = filterItemsByKeywords(keywords);
-  res.status(200).json(filteredItems);
-});
+  const keywords = req.params.keywords.split(',')
+  const filteredItems = filterItemsByKeywords(keywords)
+  res.status(200).json(filteredItems)
+})
 
 app.get('/items/location', (req, res) => {
-  const { lat, lon, radius } = req.query;
-  const filteredItems = filterItemsByLocation(lat, lon, radius);
-  res.status(200).json(filteredItems);
-});
+  const { lat, lon, radius } = req.query
+  const filteredItems = filterItemsByLocation(lat, lon, radius)
+  res.status(200).json(filteredItems)
+})
 
 app.get('/item/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  const item = getItemById(id);
+  const id = parseInt(req.params.id)
+  const item = getItemById(id)
 
   if (item) {
-    res.status(200).json(item);
+    res.status(200).json(item)
   } else {
-    res.status(404).json({ error: 'Item not found' });
+    res.status(404).json({ error: 'Item not found' })
   }
-});
+})
+
+app.get('/items/date/:date', (req, res) => {
+  const date = new Date(req.params.date)
+  const filteredItems = filterItemsByDate(date)
+  res.status(200).json(filteredItems)
+})
 
 app.delete('/item/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  const deleted = removeItem(id);
+  const id = parseInt(req.params.id)
+  const deleted = removeItem(id)
 
   if (deleted) {
-    res.status(204).send();
+    res.status(204).send()
   } else {
-    res.status(404).json({ error: 'Item not found' });
+    res.status(404).json({ error: 'Item not found' })
   }
-});
+})
 
 function filterItemsByUser(user_id) {
   return Object.values(items)
     .flat()
-    .filter((item) => item.user_id === user_id);
+    .filter((item) => item.user_id === user_id)
 }
 
 function filterItemsByKeywords(keywords) {
   return Object.values(items)
     .flat()
-    .filter((item) => keywords.some((kw) => item.keywords.includes(kw)));
+    .filter((item) => keywords.some((kw) => item.keywords.includes(kw)))
 }
 
 function filterItemsByLocation(lat, lon, radius) {
+  const center = { latitude: lat, longitude: lon }
+
   return Object.values(items)
     .flat()
-    .filter((item) => calculateDistance(lat, lon, item.lat, item.lon) <= radius);
+    .filter((item) => {
+      const itemLocation = { latitude: item.lat, longitude: item.lon }
+      const distance = geolib.getDistance(center, itemLocation)
+      return distance <= radius
+    })
 }
 
 function getItemById(id) {
   return Object.values(items)
     .flat()
-    .find((item) => item.id === id);
+    .find((item) => item.id === id)
 }
 
-app.listen(port,  () => {
-  console.log(`Example app listening on port ${port}`);
-});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
