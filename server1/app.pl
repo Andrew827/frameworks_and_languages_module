f7ce76fb8e62e988820673b9c75827f32be37254
Andrew827 <96234773+Andrew827@users.noreply.github.com>
2023-12-11 08:49:32 +0000

    Small changes, i have added documentation and comments into my code to talk about the frameworks

--- server1/app.pl
+++ server1/app.pl
@@ -4,6 +4,10 @@ use List::Util qw(first);
 use Mojolicious::Plugin::SecureCORS;
 use File::Spec;
 
+# I decided to use this framework to work in a different langague. It was challanging, but i got there. 
+# I did slightly pick it due to the name - Mojolicious ... Sounds delicous ... :). 
+
+# Set up cors headers for all routes 
 app->hook(
     before_dispatch => sub ($c) {
         $c->res->headers->header( 'Access-Control-Allow-Origin' => '*' );
@@ -15,27 +19,35 @@ app->hook(
     }
 );
 
+# Serve the index.html file that shows how to use and interact with the server
 get '/' => sub {
     my $c = shift;
     my $file_path = $c->app->home->rel_file('index.html');
     $c->reply->file($file_path);
 };
 
+# Configure Hynoptoad to listen on port 8000 - This is a producion ready web serve, it pre-forks
+# several worker processes to handle incoming requests. Its also a reference to futurama. :)
 app->config( hypnotoad => { listen => ['http://*:8000'] } );
 
+#initialse item stores and ID counter 
 my %items;
 my $next_id = 1;
 
+# Function to get the current ISO datetime format 
 sub iso_datetime {
     return Mojo::Date->new->to_datetime;
 }
 
+# Route that returns a text response 
 get '/' => sub ($c) {
     $c->render( text => 'Server - Framework Moja' );
 };
 
+# This is our create item 
 post '/item' => sub ($c) {
     my $params = $c->req->json;
+    # Validate fields 
     unless ( $params->{user_id}
         && $params->{keywords}
         && $params->{description}
@@ -47,7 +59,7 @@ post '/item' => sub ($c) {
             status => 405
         );
     }
-
+    # Create item with an ID and Time stamp
     my $new_item = {
         id          => $next_id++,
         user_id     => $params->{user_id},
@@ -58,19 +70,24 @@ post '/item' => sub ($c) {
         date_from   => iso_datetime(),
     };
 
+    # Store items and return response
     push @{ $items{ $new_item->{id} } }, $new_item;
     $c->render( json => $new_item, status => 201 );
 };
 
-# Test failure due to output being in a different format. Sorting alg needed, this way it should pass the test.
+# Retrive all items 
 
 get '/items' => sub ($c) {
 
+    # Flatten items into a list
     my @all_items = map { @$_ } values %items;
-
+    # Set the order i want the data to appear
     my @desired_order = qw(id user_id keywords description lat lon date_from);
-
+    # Sort by ID
     my @sorted_items = sort { $a->{id} <=> $b->{id} } @all_items;
+     
+     #Perl doesn't have a built-in list comprehension syntax like some other languages, but you can achieve similar results using map and grep functions.
+     #The code snippet below demonstrates a simple list comprehension to filter and format items:
 
     my @formatted_items = map {
         my %formatted_item;
@@ -78,13 +95,19 @@ get '/items' => sub ($c) {
         \%formatted_item;
     } @sorted_items;
 
+    # This was part of my code that failed all the time. My data was returning in a weird order so i needed to format the output 
+    # into the correct order 
+
     $c->render( json => \@formatted_items, status => 200 );
 };
+
+
+# Retrieve items by ID
 
 get '/item/:id' => sub ($c) {
     my $id   = $c->stash('id');
     my $item = first { $_->{id} == $id } map { @$_ } values %items;
-
+    
     if ($item) {
         $c->render( json => $item, status => 200 );
     }
@@ -93,15 +116,7 @@ get '/item/:id' => sub ($c) {
     }
 };
 
-get '/items' => sub ($c) {
-    my @all_items    = map  { @$_ } values %items;
-    my @sorted_items = sort { $a->{id} <=> $b->{id} } @all_items;
-
-    my @formatted_items = map { format_item($_) } @sorted_items;
-
-    $c->render( json => \@formatted_items, status => 200 );
-};
-
+# Utility subroutine to format an item to my desired order
 sub format_item {
     my ($item) = @_;
     my @desired_order = qw(id user_id keywords description lat lon date_from);
@@ -110,6 +125,7 @@ sub format_item {
     return \%formatted_item;
 }
 
+#Delete item by ID
 del '/item/:id' => sub ($c) {
     my $id      = $c->stash('id');
     my $deleted = remove_item($id);
@@ -122,14 +138,18 @@ del '/item/:id' => sub ($c) {
     }
 };
 
+#Handles pre flight CORS request for the root
 options '/' => sub ($c) {
     $c->rendered(204);
 };
 
+
+# Handles CORS for the /item route
 options '/item' => sub ($c) {
     $c->rendered(405);
 };
 
+# Utility subroutine to remove item by ID 
 sub remove_item ($id) {
     if ( exists $items{$id} ) {
         delete $items{$id};
@@ -142,4 +162,18 @@ sub remove_item ($id) {
     return 0;
 }
 
+#Start App
 app->start( 'daemon', '-l', 'http://localhost:8000' );
+
+# Mojolicous does support asynchronous programming to handle cocurrent requests. 
+# This is an extremely simple implemention and use of the framework. You can install
+#Mojolicous and use the command mojo generate app server. This creates a scaffold. It creates
+# a directory with multiple files and subdir. I am not advanced enough to operate the files correctly.
+# I did attempt to convert but i struggled and remained with this simple implementation. Its affective, operational
+# and works for small productions. If i wanted to use any perl within my html i would add .ep extension (embedded perl)
+
+# I learned implementation of this langague by following tutorials and documentation
+# tutorial example https://www.youtube.com/watch?v=_AInPp-dneQ, https://qntm.org/perl_en (this is a learn perl tutorial in the mojo documentation)
+# Framework docs: https://docs.mojolicious.org/
+
+
